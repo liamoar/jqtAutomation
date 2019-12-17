2f881a59975c3fbb10d0c8ffc0d513bfefe5d643
liamoar <avishr44@gmail.com>
2019-12-17 12:49:25 +0545

    refactor

--- cypress/page_objects/loginobj.js
+++ cypress/page_objects/loginobj.js
@@ -94,7 +94,6 @@ export function navigateFrontendStore(){
 
     });
 }
-
 // export function navigateCmsLogin(){
 //     cy.visit('uat-admin.jqt01.com')
 //     //send request to if the user is loggedin or not
@@ -120,24 +119,7 @@ export function navigateFrontendStore(){
 
 export function navigateCmsLogin(){
     cy.visit('uat-admin.jqt01.com')
-    // //send request to if the user is loggedin or not
-    //  var options = {
-    //      url : 'https://uat-admin.jqt01.com/system/dashboard/all',
-    //      headers: {'X-Requested-With': 'XMLHttpRequest'},
-    //      failOnStatusCode: false,
-    //  }
-
-    // cy.request(options).then((elm)=>{
-    //     if(elm.status == 401){
-    //         cy.log("User is not logged in ")
-    //         cy.get('#user-Username').type('admin')
-    //         cy.get('#user-CurrentPassword').type('123Admin@')
-    //         cy.get('.btn').wait(2000).click()
-    //     }else{
-    //         cy.log("loggedin")
-    //     }
-    //  })
-             cy.get('#user-Username').type('admin')
+            cy.get('#user-Username').type('admin')
             cy.get('#user-CurrentPassword').type('123Admin@')
             cy.get('.btn').click()
 }
