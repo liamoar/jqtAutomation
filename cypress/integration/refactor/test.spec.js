50be5161ad369b7125df503c993cef4e292b8367
liamoar <avishr44@gmail.com>
2020-01-08 09:45:06 +0545

    check json file for key exist or not

--- cypress/integration/refactor/test.spec.js
+++ cypress/integration/refactor/test.spec.js
@@ -13,13 +13,22 @@ describe('assert data', () => {
         var getid = this.userid
         cy.log(getid)
         cy.task('checkFileExists', "cypress/fixtures/test.json").then(status => {
+            //debugger
             if(!status){
                 cy.writeFile("cypress/fixtures/test.json", [{ class: getid }])
             }else{
                 cy.readFile("cypress/fixtures/test.json").then(data => {
+                    //debugger
                     if(Array.isArray(data)){
-                        data.push({ class: getid });
-                        var fileData = data;
+                        let totalKeys = data.map(function(datum){return Object.keys(datum)[0] });
+                        if(totalKeys.includes('class')){
+                            let index = totalKeys.indexOf("class");
+                            data[index].class = getid;
+                        }else{
+                            data.push({"class":getid});
+                        }
+                        
+                        fileData = data;
                     }else{
                         var fileData =  [data ,{ class: getid } ];
                     } 
